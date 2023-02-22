f66ff994b0545361b3f670f5b5a3a5512c045160
KarlP17123 <karl.park@telia.com>
2023-02-22 20:10:33 +0000

    Add Post Side and Post text

---
+++ news/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'news/index.html', context)
+
+def detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    posts = Post.objects.exclude(post_id__exact=post.post_id)[:5]
+    context = {'post': post, 'posts': posts}
+    return render(request, 'news/detail.html', context)
